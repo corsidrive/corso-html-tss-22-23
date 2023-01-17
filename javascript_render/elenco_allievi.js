d371368574eaab93764782e46c4116e6ca3d9bfc
Monna lisa <sarassoroberto@gmail.com>
2023-01-17 16:59:16 +0100

    layout ok

---
+++ javascript_render/elenco_allievi.js
@@ -0,0 +1,17 @@
+  
+        const  allievi = [
+            "Mattia Caddeo",
+            "Alana Shelley Dantas Alves Bispo",
+            "Valentina Del Monaco",
+            "Andrea Fabbricatore",
+            "Simone Greco",
+            "Enrico Malesani",
+            "Marco Martinotti",
+            "Cristina Angelica Marutiac",
+            "Fabio Miseo",
+            "Vincenzo Mola",
+            "Matteo Peiretti",
+            "Matteo Vayr",
+            "Emanuela Velardo",
+            "Cinzia Vittone"
+        ]
